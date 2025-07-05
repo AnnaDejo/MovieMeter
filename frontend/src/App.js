06b04d78e5910b168e358d0149b1e4ac0055f8ce
AnnaDejo <annadejo12a@gmail.com>
2025-07-05 05:43:43 +0530

    Created Register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,33 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Navbar from "./components/Navbar";
+
+function LayoutWrapper({ children }) {
+  const location = useLocation();
+  const hideNavbarRoutes = ["/register", "/login"]; // ⛔ no navbar here
+
+  const hideNavbar = hideNavbarRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {!hideNavbar && <Navbar />}
+      {children}
+    </>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <LayoutWrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </LayoutWrapper>
     </Router>
   );
 }
